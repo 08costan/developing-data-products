6e73b98f232974c01fabe71d3df1e4593d4be214
Vincent Costanza <costanza.vincent@gmail.com>
2015-10-23 16:04:51 +0200

    Course Project submission

--- ui.R
+++ ui.R
@@ -7,11 +7,21 @@ shinyUI(fluidPage(
 
     sidebarLayout(
         sidebarPanel(
+            h4("1) Quick-start guide:"),
+            
             p("You already checked mtcars, swiss, ... datasets in Coursera's Data Science Specialization.
             Ever wondered to have a quick look
-              at other R base datasets? Well, here is the complete list:"),
+              at other R base datasets? Well, here is the complete list."),
             
-            selectInput("dataset", "Choose a dataset:", 
+            p("Pick one dataset in the list below. A short description will immediately
+              appear below the name you selected. This app has two tabs: Table Viewer and Dataset Help page.
+              Whenever you select a new dataset, the tabs will automatically update, so you can either check
+              the contents of the dataset or its help page (taken from R documentation)."),
+            
+            
+            h4("2) Pick a dataset:"),
+            
+            selectInput("dataset","Dataset:", 
                         choices = data()$results[,3],multiple=FALSE,selectize=TRUE),
             
             p("Dataset description:"),
